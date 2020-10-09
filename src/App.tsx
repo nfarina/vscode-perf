39a27b3803d7574467691709f8cb8d5194916c14
Nick Farina <nfarina@gmail.com>
2020-10-09 11:02:36 -0700

    Add styled-components, @types/styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+
+const StyledComponent = styled.div`
+  background: blue;
+`
 
 function App() {
   return (
